cfcc29c4e23e4f1939c074386daa32042a46973a
Esteban Azael Morales Vega <azaelmorales029@gmail.com>
2021-10-15 13:57:59 -0500

    first commit

---
+++ src/main/java/com/azael/spring/HelloController.java
@@ -0,0 +1,18 @@
+package com.azael.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("goHello")
+	public ModelAndView redirectTo() {
+		ModelAndView MV = new ModelAndView();
+		MV.setViewName("hello");
+		MV.addObject("message", "Mensaje desde un controlador");
+		return MV;
+	}
+	
+}
